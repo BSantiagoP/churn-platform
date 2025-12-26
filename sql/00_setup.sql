a729fce9a2c965e6ffae1d58b6bdcf8ab7c24e75
BSantiagoP <bsp32@njit.edu>
2025-12-26 20:58:00 +0000

    feat: updated catalog name

--- sql/00_setup.sql
+++ sql/00_setup.sql
@@ -1,7 +1,7 @@
 -- sql/00_setup.sql
 
-CREATE CATALOG IF NOT EXISTS end_to_end_business_insight_pipeline;
+CREATE CATALOG IF NOT EXISTS end_to_end_churn;
 
-CREATE SCHEMA IF NOT EXISTS end_to_end_business_insight_pipeline.churn_analytics;
+CREATE SCHEMA IF NOT EXISTS end_to_end_churn.churn_analytics;
 
-CREATE VOLUME IF NOT EXISTS end_to_end_business_insight_pipeline.churn_analytics.telco_raw_data;
+CREATE VOLUME IF NOT EXISTS end_to_end_churn.churn_analytics.telco_raw_data;
